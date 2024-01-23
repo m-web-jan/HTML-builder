3e9fac240ef38e63f3eb106c9788bddd357b8594
Mark <m.web.jan@gmail.com>
2024-01-23 23:04:47 +0300

    refactor: add path to tsk 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir('03-files-in-folder\\secret-folder', (err, files) => {
+let folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => {
     if (err) throw err;
     for (let file of files){
-        fs.stat('03-files-in-folder\\secret-folder' + '/' + file, (err, stats) => {
+        fs.stat(path.join(folderPath, file), (err, stats) => {
             if (err) throw err;
             if (!stats.isDirectory()) {
                 let file_path = path.join(__dirname, file);
